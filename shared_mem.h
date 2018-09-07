02753b2bfbd09fda1812ac213dd9cc5b28e0381d
hh <545735880@qq.com>
2018-09-07 21:57:22 +0800

    commit

--- shared_mem.h
+++ shared_mem.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <sys/shm.h>
+#include <iostream>
 
 namespace smemlloc
 {
-
+//shared memmory
 class SMem
 {
-//创建
 public:
-    void *createSMem();
+    SMem(): shm_id_(0), key_(0), is_create_(false) {}
+    void *createSMem(const size_t &size);
     void *attachSMem();
     void *getFileKey();
 public:
@@ -18,8 +19,13 @@ public:
         static SMem smem;
         return smem;
     }
-
-
-}
+private:
+    //共享内存id
+    int shm_id_;
+    //键
+    key_t key_;
+    //是否成功申请到共享内存
+    bool is_create_;
+};
 
 }
